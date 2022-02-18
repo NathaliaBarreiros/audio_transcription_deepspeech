2b583e9ed32cc1966e2e6061de210ce256fd7512
Nath <nathalia.barreirosf@gmail.com>
2022-02-17 20:40:43 -0500

    poo logic updated

---
+++ deepspeech_transcript/deepspeech_transcript/main_audio_transcript_poo.py
@@ -0,0 +1,54 @@
+# %% Libraries import
+from deepspeech_transcript.preprocessing_poo import Preprocessing
+from deepspeech_transcript.preprocessing_poo import DeepSpeechModel
+import numpy as np
+import pandas as pd
+from typing import List
+from operator import index
+from deepspeech import Model
+import os
+
+
+# %%
+def main():
+
+    model: str = "~/audio_transcription_deepspeech/deepspeech_transcript/models"
+    dir_name = os.path.expanduser(model)
+    ds_instance = DeepSpeechModel(dir_name)
+    output_graph, scorer = ds_instance.resolve_models_paths()
+    model_retval: List[str] = ds_instance.load_models()
+    data = pd.read_csv("../csv_docs/paths.csv")
+    paths: List[str] = list(data.audio_path)
+
+    transcriptions: List[str] = []
+    new_data: List[str] = []
+    final_data: List[List] = []
+    header: List[str] = ["audio_path", "transcriptions"]
+    aggresive = 1
+
+    print("Running inference...")
+
+    for i in range(len(data.audio_path)):
+        audio = data.audio_path[i]
+        wave_file = audio
+        wave_instance = Preprocessing(wave_file, 30, 300, aggresive)
+        segments, sample_rate, audio_length, vad = wave_instance.vad_segment_generator(
+            wave_file, aggresive)
+        for j, segment in enumerate(segments):
+            audio = np.frombuffer(segment, dtype=np.int16)
+            output = ds_instance.transcript_audio_segments(
+                model_retval[0], audio)
+            transcript = output[0]
+        transcriptions.append(transcript)
+        new_data = [paths[i], transcriptions[i]]
+        final_data.append(new_data)
+
+    new_csv = pd.DataFrame(final_data, columns=header)
+    new_csv.to_csv("../csv_docs/transcripted_audios5.csv", index=False)
+
+    print("Audio transcriptions are done, go fetch the transcripted_audios.csv file!")
+
+
+# %%
+if __name__ == "__main__":
+    main()
--- deepspeech_transcript/deepspeech_transcript/preprocessing_poo.py
+++ deepspeech_transcript/deepspeech_transcript/preprocessing_poo.py
@@ -1,30 +1,48 @@
 import contextlib
 import wave
 import collections
+from deepspeech_transcript.preprocessing import frame_generator
 from deepspeech_transcript.preprocessing2 import read_wave
 import webrtcvad
-from typing import List, Dict, Tuple
+from typing import Generator, List, Dict, Tuple
 from deepspeech import Model
 from timeit import default_timer as timer
 import glob
 import os
 import numpy as np
-import pandas as pd
 
 """
-class Wave
+|__class Frame
+|__class Preprocessing
+    |_def read_wave (method)
+    |_def frame_generator (method)
+    |_def vad_collector (method)
+|__class DeepSpeechModel
 
 """
 
 
-class Wave:
-    def __init__(self, path: str):
+class Frame():
+    def __init__(self, bytes: bytes, timestamp: float, duration: float):
+        self.bytes = bytes
+        self.timestamp = timestamp
+        self.duration = duration
+
+
+class Preprocessing:
+    def __init__(self, path: str, frame_duration_ms: int, padding_duration_ms: int, aggressiveness: int):
         self.path = path
         self.pcm_data = self.read_wave(path)["pcm_data"]
         self.sample_rate = self.read_wave(path)["sample_rate"]
         self.duration = self.read_wave(path)["duration"]
+        self.frame_duration_ms = frame_duration_ms
+        self.padding_duration_ms = padding_duration_ms
+        self.vad = self.vad_segment_generator(path, aggressiveness)[3]
+        self.frames = list(self.frame_generator(
+            frame_duration_ms, self.pcm_data, self.sample_rate))
+        self.aggressiveness = aggressiveness
 
-    def read_wave(self, path) -> Dict:
+    def read_wave(self, path: str) -> Dict:
         with contextlib.closing(wave.open(path, "rb")) as wf:
             num_channels: int = wf.getnchannels()
             assert num_channels == 1
@@ -38,14 +56,87 @@ class Wave:
 
             return {"pcm_data": pcm_data, "sample_rate": sample_rate, "duration": duration}
 
+    def frame_generator(self, frame_duration_ms: int, pcm_data: bytes, sample_rate: int) -> Generator:
+        n = int(self.sample_rate * (frame_duration_ms/1000.0)*2)
+        offset: int = 0
+        timestamp: float = 0.0
+        duration: float = (float(n)/sample_rate) / 2.0
+        while offset + n < len(pcm_data):
+            yield Frame(self.pcm_data[offset: offset + n], timestamp, duration)
+            timestamp += duration
+            offset += n
 
-def main():
-    data = "../audios/audio1.wav"
-    wave_instance = Wave(data)
-    print(wave_instance.path)
-    print(wave_instance.sample_rate)
-    print(wave_instance.duration)
+    def vad_collector(self, sample_rate: int, frame_duration_ms: int, padding_duration_ms: int, vad: webrtcvad.Vad, frames: List[Frame]) -> Generator:
+        num_padding_frames = int(padding_duration_ms/frame_duration_ms)
+        ring_buffer = collections.deque(maxlen=num_padding_frames)
+        triggered = False
+        voiced_frames = []
+
+        for frame in frames:
+            is_speech = vad.is_speech(frame.bytes, sample_rate)
+            if not triggered:
+                ring_buffer.append((frame, is_speech))
+                num_voiced = len([f for f, speech in ring_buffer if speech])
+                if num_voiced > 0.9 * ring_buffer.maxlen:
+                    triggered = True
+                    for f, s in ring_buffer:
+                        voiced_frames.append(f)
+                    ring_buffer.clear()
+            else:
+                voiced_frames.append(frame)
+                ring_buffer.append((frame, is_speech))
+                num_unvoiced = len(
+                    [f for f, speech in ring_buffer if not speech])
+                if num_unvoiced > 0.9*ring_buffer.maxlen:
+                    triggered = False
+                    yield b"".join([f.bytes for f in voiced_frames])
+                    ring_buffer.clear()
+                    voiced_frames = []
+        if voiced_frames:
+            yield b"".join([f.bytes for f in voiced_frames])
+
+    def vad_segment_generator(self, path: str, aggressiveness: int) -> List:
+        read_instance = self.read_wave(path)
+        assert read_instance["sample_rate"] == 16000, "Only 16000Hz input WAV files are supported for now!"
+        frame_instance = self.frame_generator(
+            30, read_instance["pcm_data"], read_instance["sample_rate"])
+        frame_instance = list(frame_instance)
+        vad = webrtcvad.Vad(int(aggressiveness))
+        segments = self.vad_collector(
+            read_instance["sample_rate"], 30, 300, vad, frame_instance)
+
+        return [segments,  read_instance["sample_rate"], read_instance["duration"], vad]
 
 
-if __name__ == "__main__":
-    main()
+class DeepSpeechModel:
+    def __init__(self, dir_name: str):
+        self.dir_name = dir_name
+        self.models = self.resolve_models_paths()[0]
+        self.scorer = self.resolve_models_paths()[1]
+        self.ds = self.load_models()[0]
+
+    def resolve_models_paths(self) -> list[str, str]:
+        pb: str = glob.glob(self.dir_name+"/*.pbmm")[0]
+        scorer: str = glob.glob(self.dir_name+"/*.scorer")[0]
+        return [pb, scorer]
+
+    def load_models(self) -> list[Model, float, float]:
+        model_load_start = timer()
+        ds = Model(self.models)
+        model_load_end = timer() - model_load_start
+
+        scorer_load_start = timer()
+        ds.enableExternalScorer(self.scorer)
+        scorer_load_end = timer() - scorer_load_start
+
+        return [ds, model_load_end, scorer_load_end]
+
+    def transcript_audio_segments(self, ds: Model, audio_stt: np.ndarray) -> list[str, float]:
+        inference_time: float = 0.0
+        inference_start = timer()
+        output: str = ds.stt(audio_stt)
+
+        inference_end = timer() - inference_start
+        inference_time += inference_end
+
+        return [output, inference_time]
