bb7d2fc8ed84d3f88226bd604d620a36cd1751ee
Nath <nathalia.barreirosf@gmail.com>
2022-02-18 02:04:54 -0500

    main scripts commented

--- deepspeech_transcript/deepspeech_transcript/main_audio_transcript.py
+++ deepspeech_transcript/deepspeech_transcript/main_audio_transcript.py
@@ -13,36 +13,38 @@ def main():
     model: str = "~/audio_transcription_deepspeech/deepspeech_transcript/models"
     dir_name = os.path.expanduser(model)
 
-    aggressive = 1
-
-    # Resolve all the paths of model files
+    # Resolves all the paths of model files
     output_graph, scorer = pr.resolve_models(dir_name)
 
-    # Load output_graph, alphabet and scorer
+    # Loads output_graph and scorer
     model_retval: List[str] = pr.load_model(output_graph, scorer)
 
     data = pd.read_csv("../csv_docs/paths.csv")
+    aggressive = 1
     paths: List[str] = list(data.audio_path)
     transcriptions: List[str] = []
     new_data: List[str] = []
     final_data: List[List] = []
     header: List[str] = ["audio_path", "transcriptions"]
+
     print("Running inference...")
 
     for i in range(len(data.audio_path)):
         audio = data.audio_path[i]
         wave_file = audio
+        # Instance of Preprocessing class to generate vad segments of pcm audio
         segments, sample_rate, audio_length = pr.vad_segment_generator(
             wave_file, aggressive
         )
         for j, segment in enumerate(segments):
             audio = np.frombuffer(segment, dtype=np.int16)
-            output = pr.stt(model_retval[0], audio, sample_rate)
+            # Vad audio segments transcription
+            output = pr.stt(model_retval[0], audio)
             transcript = output[0]
         transcriptions.append(transcript)
         new_data = [paths[i], transcriptions[i]]
         final_data.append(new_data)
-
+    # Audio paths and audio's transcriptions into new CSV file
     new_csv = pd.DataFrame(final_data, columns=header)
     new_csv.to_csv("../csv_docs/transcripted_audios.csv", index=False)
 
@@ -52,5 +54,3 @@ def main():
 # %% Entry point
 if __name__ == "__main__":
     main()
-
-# %%
--- deepspeech_transcript/deepspeech_transcript/main_audio_transcript_poo.py
+++ deepspeech_transcript/deepspeech_transcript/main_audio_transcript_poo.py
@@ -1,4 +1,5 @@
-# %% Libraries import
+"""Libraries import
+"""
 from deepspeech_transcript.preprocessing_poo import Preprocessing
 from deepspeech_transcript.preprocessing_poo import DeepSpeechModel
 import numpy as np
@@ -9,13 +10,18 @@ from deepspeech import Model
 import os
 
 
-# %%
 def main():
+    """Main function
+    """
 
     model: str = "~/audio_transcription_deepspeech/deepspeech_transcript/models"
     dir_name = os.path.expanduser(model)
+    """Resolves all the paths of model files by instantiating DeepSpeechModel class
+    """
     ds_instance = DeepSpeechModel(dir_name)
     output_graph, scorer = ds_instance.resolve_models_paths()
+    """Loads output_graph and scorer into load_models method from DeepSpeechModel class
+    """
     model_retval: List[str] = ds_instance.load_models(output_graph, scorer)
     data = pd.read_csv("../csv_docs/paths.csv")
     paths: List[str] = list(data.audio_path)
@@ -31,26 +37,28 @@ def main():
     for i in range(len(data.audio_path)):
         audio = data.audio_path[i]
         wave_file = audio
+        """Instance of Preprocessing class to generate vad segments of pcm audio
+        """
         wave_instance = Preprocessing(wave_file, 30, 300, aggresive)
         segments, sample_rate, audio_length, vad = wave_instance.vad_segment_generator(
             wave_file, aggresive)
         for j, segment in enumerate(segments):
             audio = np.frombuffer(segment, dtype=np.int16)
-            # print(audio)
-            # print(type(audio))
+            """Vad audio segments transcription
+            """
             output = ds_instance.transcript_audio_segments(
                 model_retval[0], audio)
             transcript = output[0]
         transcriptions.append(transcript)
         new_data = [paths[i], transcriptions[i]]
         final_data.append(new_data)
-
+    """Audio paths and audio's transcriptions into new CSV file
+    """
     new_csv = pd.DataFrame(final_data, columns=header)
     new_csv.to_csv("../csv_docs/transcripted_audios.csv", index=False)
 
     print("Audio transcriptions are done, go fetch the transcripted_audios.csv file!")
 
 
-# %%
 if __name__ == "__main__":
     main()
